b9611b624477372f3e79e710e408886d4fb6c2de
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-07-04 08:25:51 +0530

    202. Tabs - Fetch Jobs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
+import { useState, useEffect } from 'react';
+
 const url = 'https://course-api.com/react-tabs-project';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  console.log(jobs);
+
+  if (isLoading) {
+    return (
+      <section className="job-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+
   return <h2>Tabs Starter</h2>;
 };
 export default App;
